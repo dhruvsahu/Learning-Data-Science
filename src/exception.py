487abc1154e6da4f3f54664e94358ebe7eb179a6
Dhruv Sahu <dhruvsahu2000@gmail.com>
2023-05-23 12:37:25 +0530

    important files add

---
+++ src/exception.py
@@ -0,0 +1,6 @@
+# sys library for exception handling , contains methods and variables for modifying many elements of the Python Runtime Environment
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format()
