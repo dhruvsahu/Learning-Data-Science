09dc0f121e20c5ca2da0b17c10edd2115244ce8f
Dhruv Sahu <dhruvsahu2000@gmail.com>
2023-05-30 20:48:06 +0530

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -46,13 +46,51 @@ class DataTransformation:
                 ]
             )
 
-            logging.info("Categorical columns encoding completed")
+            logging.info(f"Categorical Features: {categorical_columns}")
+            logging.info(f"Numerical Features: {numerical_columns}")
 
             preprocessor = ColumnTransformer(
                 [
                     ("num=pipepline",num_pipeline,numerical_columns)
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
                 ]
             ) 
             
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocesser object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_column_name = ["writing_score","reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1) 
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessor on training dataframe and test dataframe.")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.fit_transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing objects")
+            
+
         except:
             pass
